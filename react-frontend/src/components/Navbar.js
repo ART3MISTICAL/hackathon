b44839d7c89abb7dcc0a5e8c4986fa010537d6cb
ART3MISTICAL <dev.marvel.avengers@gmail.com>
2022-08-09 14:21:42 +0530

    dark mode

--- react-frontend/src/components/Navbar.js
+++ react-frontend/src/components/Navbar.js
@@ -36,7 +36,7 @@ export default function Navbar(props) {
 					<li>
 						<div className="form-check form-switch text-light my-2">
   						<input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-  						<label className="form-check-label" for="flexSwitchCheckDefault">Dark Mode</label>
+  						<label className="form-check-label" for="flexSwitchCheckDefault" id="dark-mode-label">Dark Mode</label>
 						</div>
 					</li>
 					<li className="nav-item" id="contact">
