95a2b0630994a7ee466afa0f4199ca083b74b36d
ART3MISTICAL <dev.marvel.avengers@gmail.com>
2022-08-12 13:06:04 +0530

    all logic done

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -3,16 +3,9 @@ import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 //import SearchBar from './components/SearchBar';
-import rainy from  "./assets/rain.png";
-import cloudy from "./assets/cloud.jpg";
 import About from './components/About';
 import Home from './components/Home';
-import cloudyPhone from './assets/earth2.png';
-import rainyPhone from './assets/rain2.png';
-import sunny from './assets/sunny.jpg';
-import sunnyPhone from './assets/sunny2.jpg';
-import clear from './assets/clear.jpg';
-import clearPhone from './assets/blue2.png';
+
 import {
   BrowserRouter,
   Routes,
@@ -26,64 +19,7 @@ function App() {
 
 	let weather = "clear";
 
-	if (weather === 'rainy') {
-		if (window.matchMedia("(max-width: 600px)").matches) {
-			document.body.style.backgroundImage = `url(${rainyPhone})`;
-			document.body.style.backgroundSize = "cover";
-			document.body.style.backgroundRepeat = "no-repeat";		
-		} else {
-			document.body.style.background = `rgba(0,0,0,0.2)url(${rainy})`;
-			document.body.style.backgroundSize = "cover";
-			document.body.style.backgroundRepeat = "repeat-y";
-			document.body.style.backgroundBlendMode = "darken";
-		}
-		
-	}
-	else if (weather === 'cloudy') {
-		if (window.matchMedia("(max-width: 600px)").matches) {
-			document.body.style.background = `rgba(0,0,0,0.2)url(${cloudyPhone})` ;
-			document.body.style.backgroundSize = "cover";
-			document.body.style.backgroundRepeat = "no-repeat";
-			document.body.style.backgroundBlendMode = "darken";
 
-		} else {
-		document.body.style.background = `rgba(0,0,0,0.2)url(${cloudy})` ;
-		document.body.style.backgroundSize = "cover";
-		document.body.style.backgroundRepeat = "repeat-y";
-		document.body.style.backgroundBlendMode = "darken";
-
-	}
-	}
-
-	else if (weather === 'sunny') {
-		if (window.matchMedia("(max-width: 600px)").matches) {
-			document.body.style.background = `rgba(0,0,0,0.2)url(${sunnyPhone})` ;
-			document.body.style.backgroundSize = "cover";
-			document.body.style.backgroundRepeat = "no-repeat";
-			document.body.style.backgroundBlendMode = "darken";
-
-		} else {
-		document.body.style.background = `rgba(0,0,0,0.2)url(${sunny})` ;
-		document.body.style.backgroundSize = "cover";
-		document.body.style.backgroundRepeat = "repeat-y";
-		document.body.style.backgroundBlendMode = "darken";
-	}
-	}
-	
-	else if (weather === 'clear') {
-		if (window.matchMedia("(max-width: 600px)").matches) {
-			document.body.style.background = `rgba(0,0,0,0.2)url(${clearPhone})` ;
-			document.body.style.backgroundSize = "cover";
-			document.body.style.backgroundRepeat = "no-repeat";
-			document.body.style.backgroundBlendMode = "darken";
-
-		} else {
-		document.body.style.background = `rgba(0,0,0,0.2)url(${clear})` ;
-		document.body.style.backgroundSize = "cover";
-		document.body.style.backgroundRepeat = "repeat-y";
-		document.body.style.backgroundBlendMode = "darken";
-	}
-	}
 
   return (
     <div className="App">
--- react-frontend/src/components/Home.jsx
+++ react-frontend/src/components/Home.jsx
@@ -1,21 +1,130 @@
-import React from 'react';
-import SearchBar from './SearchBar';
+import React, { useState } from "react";
+import axios from "axios";
+import cloudyPhone from '../assets/earth2.png';
+import rainyPhone from '../assets/rain2.png';
+import sunny from '../assets/sunny.jpg';
+import sunnyPhone from '../assets/sunny2.jpg';
+import clear from '../assets/clear.jpg';
+import clearPhone from '../assets/blue2.png';
+import dust from '../assets/dust.jpg'
+import dustPhone from '../assets/dust2.jpeg';
+import rainy from  "../assets/rain.png";
+import cloudy from "../assets/cloud.jpg";
+//import SearchBar from './SearchBar';
 
 export default function Home() {
+	
+	const [data, setData] = useState({})
+	const [location, setLocation] = useState('')
+
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
+
+	const searchLocation = (event) => {
+	  if (event.key === 'Enter') {
+		axios.get(url).then((response) => {
+		  setData(response.data)
+		  console.log(response.data)
+			let weather = data.weather[0].main;
+
+			if (weather === 'Rain') {
+				if (window.matchMedia("(max-width: 600px)").matches) {
+					document.body.style.backgroundImage = `url(${rainyPhone})`;
+					document.body.style.backgroundSize = "cover";
+					document.body.style.backgroundRepeat = "no-repeat";		
+				} else {
+					document.body.style.background = `rgba(0,0,0,0.2)url(${rainy})`;
+					document.body.style.backgroundSize = "cover";
+					document.body.style.backgroundRepeat = "repeat-y";
+					document.body.style.backgroundBlendMode = "darken";
+				}
+				
+			}
+			else if (weather === 'Clouds') {
+				if (window.matchMedia("(max-width: 600px)").matches) {
+					document.body.style.background = `rgba(0,0,0,0.2)url(${cloudyPhone})` ;
+					document.body.style.backgroundSize = "cover";
+					document.body.style.backgroundRepeat = "no-repeat";
+					document.body.style.backgroundBlendMode = "darken";
+		
+				} else {
+				document.body.style.background = `rgba(0,0,0,0.2)url(${cloudy})` ;
+				document.body.style.backgroundSize = "cover";
+				document.body.style.backgroundRepeat = "repeat-y";
+				document.body.style.backgroundBlendMode = "darken";
+		
+			}
+			}
+		
+			else if (weather === 'Sun') {
+				if (window.matchMedia("(max-width: 600px)").matches) {
+					document.body.style.background = `rgba(0,0,0,0.2)url(${sunnyPhone})` ;
+					document.body.style.backgroundSize = "cover";
+					document.body.style.backgroundRepeat = "no-repeat";
+					document.body.style.backgroundBlendMode = "darken";
+		
+				} else {
+				document.body.style.background = `rgba(0,0,0,0.2)url(${sunny})` ;
+				document.body.style.backgroundSize = "cover";
+				document.body.style.backgroundRepeat = "repeat-y";
+				document.body.style.backgroundBlendMode = "darken";
+			}
+			}
+			
+			else if (weather === 'Clear') {
+				if (window.matchMedia("(max-width: 600px)").matches) {
+					document.body.style.background = `rgba(0,0,0,0.2)url(${clearPhone})` ;
+					document.body.style.backgroundSize = "cover";
+					document.body.style.backgroundRepeat = "no-repeat";
+					document.body.style.backgroundBlendMode = "darken";
+		
+				} else {
+				document.body.style.background = `rgba(0,0,0,0.2)url(${clear})` ;
+				document.body.style.backgroundSize = "cover";
+				document.body.style.backgroundRepeat = "repeat-y";
+				document.body.style.backgroundBlendMode = "darken";
+			}
+			}
+
+			else if (weather === 'Dust') {
+				if (window.matchMedia("(max-width: 600px)").matches) {
+					document.body.style.background = `rgba(0,0,0,0.2)url(${dustPhone})` ;
+					document.body.style.backgroundSize = "cover";
+					document.body.style.backgroundRepeat = "no-repeat";
+					document.body.style.backgroundBlendMode = "darken";
+		
+				} else {
+				document.body.style.background = `rgba(0,0,0,0.2)url(${dust})` ;
+				document.body.style.backgroundSize = "cover";
+				document.body.style.backgroundRepeat = "repeat-y";
+				document.body.style.backgroundBlendMode = "darken";
+			}
+			}
+		})
+		setLocation('')
+	  }
+	}
+
 	return(
 		<>
 			<div className="container">
-				<SearchBar />
+				<input 
+					value={location}
+					type="text" 
+					className='search-bar' 
+					placeholder='Search your city here'
+					onChange={event => setLocation(event.target.value)} 
+					onKeyPress={searchLocation}
+				/>
 			</div>
 			<div className="container" id="weather-box">
 				<p id="temp">
-					15°c
+					{data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
 				</p>
 				<p id="weather">
-					Cloudy
+					{data.weather ? <p>{data.weather[0].main}</p> : null}
 				</p>
 				<p id="place">
-					Delhi, India
+					{data.name}
 				</p>
 			</div>
 		</>
