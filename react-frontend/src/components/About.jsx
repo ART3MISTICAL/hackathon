6236d920e3ba1b7768229136ec64c33627c63ea0
ART3MISTICAL <dev.marvel.avengers@gmail.com>
2022-08-12 21:26:54 +0530

    about us

--- react-frontend/src/components/About.jsx
+++ react-frontend/src/components/About.jsx
@@ -2,9 +2,28 @@ import React from 'react';
 
 export default function About() {
 	return (
-		<div className="container">
-			<p>
-				This is the about page
+		<div className="container text-box">
+			<h1 className='text'>About The Website</h1>
+			<p className='text'>
+			This website is a tool for people who need to track weather forcasts and currect conditions of the climate as they update in real time.
+			</p>
+			<p className='text'>
+			This App tells you the-
+			<ul>
+				<li>
+					Temperature 
+				</li>
+				<li>
+					Weather condition
+				</li>
+			</ul>
+			</p>
+			<h1 className="text">
+				About Us
+			</h1>
+			<p className="text">
+			Developed by Devansh Arora and  Aryan Saraf [DPS Faridabad, Sec-19] in less than 2 days as a hackathon project.
+We both have great interest in coding and believe that the future ahead truly depends on code and Tech.
 			</p>
 		</div>
 	);
